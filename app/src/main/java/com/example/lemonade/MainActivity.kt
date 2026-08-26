19786dc39cdcc4f9b5c46f597c166fe5124c47e8
Cheick Ouattara <cheickiouattata@gmail.com>
2026-08-26 16:08:58 +0000

    Ajout d'une couleur de fond et de coins arrondis à l'image cliquable

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,6 +29,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lemonade.ui.theme.LemonadeTheme
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -81,16 +84,21 @@ fun LemonadeWithTextImgAndBtn(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
     ) {
+
         Image(
-            painter = painterResource(images),
-            contentDescription = stringResource(imageContentDescription),
-            modifier = Modifier.clickable {
-                if (etat == 4) {
-                    etat = 1
-                } else {
-                    etat = etat + 1
-                }
-            },
+                painter = painterResource(images),
+                contentDescription = stringResource(imageContentDescription),
+                modifier = Modifier
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(Color(0xFFC3ECD2))
+                    .clickable {
+                        if (etat == 4) {
+                            etat = 1
+                        } else {
+                            etat = etat + 1
+                        }
+                    }
+                    .padding(19.dp),
         )
         Spacer(modifier = Modifier.height(16.dp))
         Text(text = stringResource(textDescription))
